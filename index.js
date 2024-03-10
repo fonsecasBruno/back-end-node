4f765766e230da5de8acfcbb743b326bf22dc5fa
fonsecasBruno <brunofsouza669@gmail.com>
2024-03-10 15:34:04 -0300

    lista1

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ app.use(express.urlencoded({extended: false}))
 
 const lista1 = require('./routes/lista1')
 const lista2 = require('./routes/lista2')
+const lista3 = require('./routes/lista3')
 
 app.use('/lista1', lista1)
 app.use('/lista2', lista2)
+app.use('/lista3', lista3)
 
 app.get('/', function(req, res){
     res.send('Rota Principal')
@@ -16,20 +18,6 @@ app.get('/', function(req, res){
 
 app.get('/hello', function(req, res){
     res.send('Chegou a resposta')
-})
-
-
-app.post('/lista2/ex2', function(req, res){
-    
-    const{nome, horasTrabalhadas, valorhora, filhos} = req.body
-
-    const salariobruto = horasTrabalhadas * valorhora
-    const acrescimo = 3/100 * salariobruto * filhos
-    const salariofinal = salariobruto + acrescimo
-    
-    res.json({salariofinal})
-
-
 })
 
 app.listen(3000, function(){
